e3442581bbd3c1ffa85be84ff25e9e9e4ce06cd0
Miles Yucht <mgyucht@gmail.com>
2012-06-14 10:19:31 -0400

    updated README, still fixing energy function

--- Summer_Internship/testMiles/funcd.h
+++ Summer_Internship/testMiles/funcd.h
@@ -26,6 +26,9 @@ const int length=40;
 
 const double RESTLEN = 1;
 
+bool isiMax, isjMax, isjMin;
+int iMax, jMax;
+
 struct Funcd {
 	
     // The () operator (function call) is now used to calculate the energy of
@@ -36,8 +39,8 @@ struct Funcd {
         
 		double funcvalue = 0;
         
-        int iMax = netSize - 1;
-        int jMax = netSize - 1;
+        iMax = netSize - 1;
+        jMax = netSize - 1;
         
         for(int i=0; i <= iMax; i++) {
             for (int j = 0; j <= jMax; j++) {
@@ -55,35 +58,32 @@ struct Funcd {
                 // also take into account strain.
                 
                 Node **nPtr = new Node*[4];
-                bool isiMax = i == iMax, isjMax = j == jMax;
+                isiMax = i == iMax;
+                isjMax = j == jMax;
+                isjMin = j == 0;
                 
                 nPtr[0] = x[i][j];
                 
                 // PBC is NOT WORKING... :P
                 
-                if (!isiMax)
-                    nPtr[1] = x[i][j + 1];
-                else
-                    nPtr[1] = x[i][0];
+                int i1 = isiMax ? 0 : i;
+                int j1 = isjMax ? 0 : j;
+                int j2 = isjMin ? jMax : j;
                 
-                if (!isjMax)
-                    nPtr[2] = x[i + 1][j];
-                else 
-                    nPtr[2] = x[0][j];
+                nPtr[1] = x[i][j1];
+                nPtr[2] = x[i1][j];
+                nPtr[3] = x[i1][j2];
                 
-                if (!(isiMax && isjMax))
-                    nPtr[3] = x[i + 1][j - 1];
-                else
-                    nPtr[3] = x[0][0];
-                
-                for (int k = 0; k < 3; k++) {
+                for (int k = 1; k < 4; k++) {
                     
                     funcvalue += 0.5 * nPtr[k]->sprstiff[k] / nPtr[k]->restlen[k] 
-                                    * euclDistSqrd(nPtr[k]->position, nPtr[0]->position);
+                                    * euclDistSqrd(nPtr[k]->position, nPtr[0]->position, k);
                      
-                    printf("%d, %d, %2f, %2f, %3f\n", i, j, nPtr[k]->sprstiff[k], nPtr[k]->restlen[k], 
-                            euclDistSqrd(nPtr[k]->position, nPtr[0]->position));
+                    printf("%d, %d, %.3f, %.3f, %6.3f | ", i, j, nPtr[k]->position[0], nPtr[0]->position[0], 
+                            euclDistSqrd(nPtr[k]->position, nPtr[0]->position, k));
                 }
+                
+                printf("\n");
             }
 		}
         
@@ -109,9 +109,29 @@ struct Funcd {
         }
     }
     
-    double euclDistSqrd(double *vec1, double *vec2) {
-        return ((vec2[0] - vec1[0]) * (vec2[0] - vec1[0]) 
-                + (vec2[1] - vec1[1]) * (vec2[1] - vec1[1]));
+    double euclDistSqrd(double *vec1, double *vec2, int k) {
+        
+        bool xShiftR = false, xShiftL = false, yShiftU = false;
+        
+        switch (k) {
+            case 1: 
+                if (isjMax) xShiftR = true;
+                break;
+            case 2: 
+                if (isiMax) yShiftU = true;
+                break;
+            case 3:
+                if (isiMax) yShiftU = true;
+                if (isjMax) xShiftL = true;
+                break;
+        }
+        
+        double xadj = xShiftR ? jMax * RESTLEN : 0 + xShiftL ? -jMax * RESTLEN : 0;
+        double yadj = yShiftU ? iMax * RESTLEN : 0;
+        double x = vec2[0] - vec1[0];
+        double y = vec2[1] - vec1[1];
+        
+        return (x - xadj) * (x - xadj) + (y - yadj) * (y - yadj);
     }
 };
 
