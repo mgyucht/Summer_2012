339ca94e47cd2053f9d3cd339e751d30332b50dc
Miles Yucht <mgyucht@gmail.com>
2012-07-05 16:53:47 -0400

    bleugh

---
+++ Summer_Internship/integrator/headers/compileinfo.h
@@ -0,0 +1,12 @@
+#ifndef COMPILEINFO_H_
+#define COMPILEINFO_H_
+// compileinfo.h
+// -------------
+// compileinfo.h contains information specific to the computer the program is 
+// being compiled on. This controls output path information primarily.
+
+#include <string>
+
+const std::string output_path = "/media/sdb1/Summer_2012/Summer_Internship/integrator/output";
+
+#endif /*COMPILEINFO_H_*/
