ef7e253c04578780c268088aebdf6f1203f77468
Miles Yucht <mgyucht@gmail.com>
2012-07-10 13:44:00 -0400

    fixing .gitignore

---
+++ Summer_Internship/integrator/headers/compileinfo.h
@@ -0,0 +1,12 @@
+#ifndef COMPILEINFO_H_
+#define COMPILEINFO_H_
+// compileinfo.h
+// -------------
+// Not to be distributed with source. This contains system-specific information
+// for path determination at compilation.
+
+// Uncomment the appropriate line. Only one can be uncommented.
+// #define HOME_COMPUTER 0
+#define DELLA 1
+
+#endif /*COMPILEINFO_H_*/
